68961492a2ef1cf67a1f3a3232843157d4cb39b3
Ivan-dp <ivan-dp@bk.ru>
2022-04-25 19:49:29 +0300

    created increment button with a counter in RedSquare

--- react-course/src/components/RedSquare.jsx
+++ react-course/src/components/RedSquare.jsx
@@ -2,8 +2,18 @@ import React, {useState} from 'react';
 
 const RedSquare = function () {
 
+    const [count, setCount] = useState(0)
+
+    function increment () {
+        setCount(count + 1);
+        console.log(count);
+    }
+
     return (
-        <div className="RedSquare"></div>
+        <div className="RedSquare">
+            <button onClick={increment}className="RedSquare__button-increment">+</button>
+            <h1>{count}</h1>
+        </div>
     )
 }
 
