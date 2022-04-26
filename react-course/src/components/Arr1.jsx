a31e5dca402e62e2a80d9a53c15eec5cd99adc1f
Ivan-dp <ivan-dp@bk.ru>
2022-04-26 17:55:40 +0300

    #3

--- react-course/src/components/Arr1.jsx
+++ react-course/src/components/Arr1.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
 export const array1 = ['apple', 10, 'orange', 4, 'strawberry', 3.15, 5.16, -1, -9, 300];
+export const copyArr1 = array1.slice(0);
+
 const Arr1 = array1.map((array1) => 
     <li className="RedSquare">{array1}</li>
 )
- 
+console.log(Arr1);
+console.log(array1);
+console.log(copyArr1);
+
 export default Arr1;
--- react-course/src/functions/ArrOfObj.jsx
+++ react-course/src/functions/ArrOfObj.jsx
@@ -1,4 +1,6 @@
 import array1 from '../components/Arr1';
+import Arr1 from '../components/Arr1';
+import copyArr1 from '../components/Arr1';
 
 const array2 = ['red', 'orange', 'blue', 'yellow', 'green', 'black', 'grey', 'purple', 'coral', 'pink'];
 
@@ -7,12 +9,13 @@ function ArrOfObj(arr1, arr2) {
     for(let i = 0; i < arr1.length; i++) {
         arr[i] = {value: arr1[i], color: arr2[i]};
     }
-    // console.log(arr);
     return arr;
 }
-// console.log(ArrOfObj);
 
-const newArr = ArrOfObj(array1, array2);
+const newArr = ArrOfObj(copyArr1, array2);
 console.log(newArr);
+console.log(Arr1);
+console.log(array1);
+console.log(copyArr1);
 
 export default ArrOfObj;
