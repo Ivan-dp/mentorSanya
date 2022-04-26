4c5294c5c5b7c19707f2c58ec2bac2abe159808a
Ivan-dp <ivan-dp@bk.ru>
2022-04-26 15:27:29 +0300

    #2 created an array and passed each of its values to a RedSquare

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from "./components/Counter";
 import RedSquare from './components/RedSquare';
+import Arr1 from'./components/Arr1';
 import './styles/App.css';
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
     <div className="App">
       {/* <ClassCounter/> */}
       {/* TODO тут можно писать штуки, чтоб не забывать что-нибудь*/}
-      <RedSquare/>
+      {/* <RedSquare/> */}
+      <ul>{Arr1}</ul>
     </div>
   );
 }
