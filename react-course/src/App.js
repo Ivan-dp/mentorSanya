a15ee3567fb06c17bb0314ac77dc9ece15c5a49e
Ivan-dp <ivan-dp@bk.ru>
2022-04-29 17:33:48 +0300

    #8 create function 'renderOtherNumArrays' for rendering arrayNum. Deleted component 'NumArray'.

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import {NumArray} from './components/NumArray'
+import { arrayNum } from './const/consts';
+import { renderOtherNumArrays } from './functions/renderOtherNumArrays';
 import './styles/App.css';
 
 function App() {
 
   return (
     <div className="App">
-      <ul>{NumArray}</ul>
+      <ul>{renderOtherNumArrays(arrayNum)}</ul>
     </div>
   );
 }
+
+console.log(renderOtherNumArrays(arrayNum));
 
 export default App;
