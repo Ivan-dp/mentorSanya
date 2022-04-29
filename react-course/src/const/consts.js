48288426a77b376e5863dc45f4d2e4b1c63e9ae0
Ivan-dp <54504811+Ivan-dp@users.noreply.github.com>
2022-04-30 00:11:53 +0300

    8 issue (#11)
    * #5 filter array of objects by number at value. In progress
    
    * #5 is done. Completed a function that removes strings from an array of objects and converts it into an array of numbers. Displayed an array of numbers on the screen with different backgrounds in accordance with the task
    
    * #5 created the sortAscend function to sort the copyArrayColorValue array in ascending order of numbers
    
    * #8 create function 'renderOtherNumArrays' for rendering arrayNum. Deleted component 'NumArray'.

--- react-course/src/const/consts.js
+++ react-course/src/const/consts.js
@@ -1,10 +1,14 @@
 import { mergeArrays } from "../functions/mergeArrays";
+import { stringsDelete } from "../functions/stringsDelete";
+import { sortAscend } from "../functions/sortAscend";
 
 export const arrayValue = ['apple', 10, 'orange', 4, 'strawberry', 3.15, 5.16, -1, -9, 300];
 export const arrayColor = ['red', 'orange', 'blue', 'yellow', 'green', 'black', 'grey', 'purple', 'coral', 'pink'];
 export const copyArrayValue = [...arrayValue];
 export const arrayColorValue = mergeArrays(copyArrayValue, arrayColor);
 export const copyArrayColorValue = [...arrayColorValue];
+
+export const arrayNum = stringsDelete(copyArrayColorValue).sort(sortAscend);
 
 export const numArrayColors = {
     '<0': 'red',
---
+++ react-course/src/functions/renderOtherNumArrays.jsx
@@ -0,0 +1,20 @@
+import { numArrayColors } from "../const/consts";
+
+export const renderOtherNumArrays = function(arr) {
+    return arr.map(item => {
+        if ((item >= 0) && (item <= 5)) {
+            return <li key={item} className="square" style={{backgroundColor: numArrayColors["0-5"]}}>
+                {item}
+            </li>;
+        } else if (item > 5) {
+            return <li key={item} className="square" style={{backgroundColor: numArrayColors[">5"]}}>
+                {item}
+            </li>;
+        }
+        return (
+            <li key={item} className="square" style={{backgroundColor: numArrayColors["<0"]}}>
+                {item}
+            </li>
+        );
+    })        
+};  
