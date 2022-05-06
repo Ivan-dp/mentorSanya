722e9f19966ca8f210a73f74444d03cfb20d3798
Ivan-dp <54504811+Ivan-dp@users.noreply.github.com>
2022-05-07 01:31:30 +0300

    12 issue (#21)
    * #5 filter array of objects by number at value. In progress
    
    * #5 is done. Completed a function that removes strings from an array of objects and converts it into an array of numbers. Displayed an array of numbers on the screen with different backgrounds in accordance with the task
    
    * #5 created the sortAscend function to sort the copyArrayColorValue array in ascending order of numbers
    
    * #8 create function 'renderOtherNumArrays' for rendering arrayNum. Deleted component 'NumArray'.
    
    * #7 configured eslint and prettier. In progress
    
    * #7 made the initial setup of eslint and prettier
    
    * #7.1 made the initial setup of eslint and prettier
    
    * #7 renamed .jsx functions at .js functions. Imported react at 'renderOtherNumArrays.js'. Fixed bugs by prettier and eslint
    
    * #9 create FuncWithNum.js
    
    * #9 Trying to pass a function to the props of a functional component
    
    * #9 issue is done. FuncWithNum.jsx works good. Passed a function to the props of a functional component
    
    * #12 add useState for toggle true/false for rendering FuncWithNum
    
    * #12 fixed the code according to the mentor's comments

--- react-course/src/components/FuncWithNum.jsx
+++ react-course/src/components/FuncWithNum.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import { React, useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const FuncWithNum = (props) => {
+  const [toggle, setToggle] = useState(true);
+  const { result } = props;
+  console.log({ result });
+
   FuncWithNum.propTypes = {
     result: PropTypes.string.IsRequired,
     number: PropTypes.string.IsRequired,
     func: PropTypes.string.IsRequired,
   };
-  return (
-    <div className="result square coral">
-      <p>ID: {props.result.id}</p>
-      <h3>Number: {props.result.number}</h3>
-      <h1>Result: {props.result.func(props.result.number)}</h1>
-    </div>
-  );
+
+  if (toggle) {
+    return (
+      <button onClick={() => setToggle(!toggle)} className="result square coral">
+        <p>ID: {props.result.id}</p>
+        <h3>Number: {props.result.number}</h3>
+        <h1>Result: {props.result.func(props.result.number)}</h1>
+      </button>
+    );
+  } else {
+    return <></>;
+  }
 };
