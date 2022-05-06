871073a4fcb532ac1773c13dc2a7d1afa436e778
Ivan-dp <54504811+Ivan-dp@users.noreply.github.com>
2022-05-06 12:24:27 +0300

    9 issue (#17)
    * #5 filter array of objects by number at value. In progress
    
    * #5 is done. Completed a function that removes strings from an array of objects and converts it into an array of numbers. Displayed an array of numbers on the screen with different backgrounds in accordance with the task
    
    * #5 created the sortAscend function to sort the copyArrayColorValue array in ascending order of numbers
    
    * #8 create function 'renderOtherNumArrays' for rendering arrayNum. Deleted component 'NumArray'.
    
    * #7 configured eslint and prettier. In progress
    
    * #7 made the initial setup of eslint and prettier
    
    * #7.1 made the initial setup of eslint and prettier
    
    * #7 renamed .jsx functions at .js functions. Imported react at 'renderOtherNumArrays.js'. Fixed bugs by prettier and eslint
    
    * #9 create FuncWithNum.js
    
    * #9 Trying to pass a function to the props of a functional component
    
    * #9 issue is done. FuncWithNum.jsx works good. Passed a function to the props of a functional component
    
    * #9.1 create new f.component with two numbers and function. Render on App.js

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,15 +1,64 @@
-import React from 'react';
-import { arrayNum } from './const/consts';
-import { renderOtherNumArrays } from './functions/renderOtherNumArrays';
+import React, { useState } from 'react';
+// eslint-disable-next-line import/no-unresolved
+import { FuncWithNum } from './components/FuncWithNum';
+// eslint-disable-next-line import/no-unresolved
+import { FuncWithNum2 } from './components/FuncWithNum2';
 import './styles/App.css';
 
 function App() {
+  const [results] = useState([
+    {
+      id: 1,
+      number: 20,
+      func: function (x) {
+        return x / 2;
+      },
+    },
+    {
+      id: 2,
+      number: 5,
+      func: function (x) {
+        return x * 2;
+      },
+    },
+    {
+      id: 3,
+      number: 8,
+      func: function (x) {
+        return x + 2;
+      },
+    },
+    {
+      id: 4,
+      number: 12,
+      func: function (x) {
+        return x - 2;
+      },
+    },
+    {
+      id: 5,
+      number: 32,
+      func: function (x) {
+        return x % 11;
+      },
+    },
+  ]);
+
   return (
     <div className="App">
-      <ul>{renderOtherNumArrays(arrayNum)}</ul>
+      {results.map((result) => (
+        <FuncWithNum result={result} key={result.id} number={result.number} func={result.func} />
+      ))}
+      <FuncWithNum2
+        key={1}
+        arg1={3}
+        arg2={19}
+        func={function (x, y) {
+          return x + y;
+        }}
+      />
     </div>
   );
 }
 
-console.log(renderOtherNumArrays(arrayNum));
 export default App;
---
+++ react-course/src/components/FuncWithNum.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const FuncWithNum = (props) => {
+  FuncWithNum.propTypes = {
+    result: PropTypes.string.IsRequired,
+    number: PropTypes.string.IsRequired,
+    func: PropTypes.string.IsRequired,
+  };
+  return (
+    <div className="result square coral">
+      <p>ID: {props.result.id}</p>
+      <h3>Number: {props.result.number}</h3>
+      <h1>Result: {props.result.func(props.result.number)}</h1>
+    </div>
+  );
+};
