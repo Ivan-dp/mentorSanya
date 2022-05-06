a1d6a49c839848cf44fe53a3632fbf6dd083ad36
Ivan-dp <ivan-dp@bk.ru>
2022-05-06 07:02:05 +0300

    #12 add useState for toggle true/false for rendering FuncWithNum

--- react-course/src/components/FuncWithNum.jsx
+++ react-course/src/components/FuncWithNum.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import { React, useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const FuncWithNum = (props) => {
+  const [toggle, setToggle] = useState(true);
+  console.log(toggle);
+
   FuncWithNum.propTypes = {
     result: PropTypes.string.IsRequired,
     number: PropTypes.string.IsRequired,
     func: PropTypes.string.IsRequired,
   };
-  return (
-    <div className="result square">
-      <p>ID: {props.result.id}</p>
-      <h3>Number: {props.result.number}</h3>
-      <h1>Result: {props.result.func(props.result.number)}</h1>
-    </div>
-  );
+
+  if (toggle == true) {
+    return (
+      <button onClick={() => setToggle(toggle != toggle)} className="result square">
+        <p>ID: {props.result.id}</p>
+        <h3>Number: {props.result.number}</h3>
+        <h1>Result: {props.result.func(props.result.number)}</h1>
+      </button>
+    );
+  } else {
+    return null;
+  }
 };
