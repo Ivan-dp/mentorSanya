0b9add3a40bf24bf6c742193c4eac2e6a644bfd5
Ivan-dp <ivan-dp@bk.ru>
2022-05-12 18:11:43 +0300

    #13 completed the functional component TestUseEffect

--- react-course/src/components/TestUseEffect.jsx
+++ react-course/src/components/TestUseEffect.jsx
@@ -2,20 +2,24 @@ import { React, useEffect, useState } from 'react';
 
 export const TestUseEffect = () => {
   const [count, setCount] = useState(0);
-
-  const myArr = [];
+  const [arr, setArr] = useState([]);
 
   useEffect(() => {
-    console.log('Я родился ' + count + ' раз!');
-    // for (var i = 0; i <= count; i += 1) {
-    // }
-    myArr.push(count + ' обезьянка');
-    console.log(myArr);
-  });
-  // console.log(myArr);
+    if (count > 0) {
+      arr.push(count + ' обезъянка');
+      console.log('Я родился ' + count + ' раз!');
+      console.log(arr);
+    }
+  }, [count, arr, setArr]);
+
   return (
     <div>
-      <button className="square green" onClick={() => setCount(count + 1)}>
+      <button
+        className="square green"
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
         Fuck Yeah!
       </button>
     </div>
