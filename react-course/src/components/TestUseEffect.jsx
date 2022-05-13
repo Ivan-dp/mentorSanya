acd46df6f52dfcd0d05dc5a83f9b09e76fa84c6e
Ivan-dp <ivan-dp@bk.ru>
2022-05-13 17:02:26 +0300

    #13 completed fComponent TestUseEffect by using spread operator

--- react-course/src/components/TestUseEffect.jsx
+++ react-course/src/components/TestUseEffect.jsx
@@ -2,19 +2,22 @@ import { React, useEffect, useState } from 'react';
 
 export const TestUseEffect = () => {
   const [count, setCount] = useState(0);
+  // eslint-disable-next-line no-unused-vars
   const [arr, setArr] = useState([]);
 
   useEffect(() => {
     if (count > 0) {
-      arr.push(count + ' обезъянка');
+      setArr([...arr, count + ' обезъянка']);
       //TODO што-то тут не так
     }
   }, [count]);
 
   useEffect(() => {
+    if (count > 0) {
       console.log('Я родился ' + count + ' раз!');
       console.log(arr);
-      //TODO почему при изменении arr, не срабатывает этот хук?
+    }
+    //TODO почему при изменении arr, не срабатывает этот хук?
   }, [arr]);
 
   return (
