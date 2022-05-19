54a50b78789601a3d326c04ad5071bd25236841c
Ivan-dp <ivan-dp@bk.ru>
2022-05-19 15:03:44 +0300

    #16 fix for pretty

--- react-course/src/components/Baby.jsx
+++ react-course/src/components/Baby.jsx
@@ -1,17 +1,14 @@
-// eslint-disable-next-line no-unused-vars
 import { React } from 'react';
 import PropTypes from 'prop-types';
 
 export const Baby = (props) => {
   Baby.propTypes = {
-    className: PropTypes.string.isRequired,
-    func: PropTypes.string.isRequired,
+    func: PropTypes.func.isRequired,
     parentColor: PropTypes.string.isRequired,
     babyColor: PropTypes.string.isRequired,
   };
-  console.log(props.parentColor, props.babyColor, props.func);
   return (
-    <div className={'square ' + props.className} style={{ backgroundColor: props.parentColor }}>
+    <div className="square" style={{ backgroundColor: props.parentColor }}>
       <button
         style={{ backgroundColor: props.babyColor }}
         onClick={() => props.func(props.babyColor)}
--- react-course/src/components/Parent.jsx
+++ react-course/src/components/Parent.jsx
@@ -1,16 +1,13 @@
 import { React, useState } from 'react';
-// eslint-disable-next-line import/no-unresolved
-import { Baby } from './Baby';
+import { Baby } from './Baby.jsx';
 
 export const Parent = () => {
-  // eslint-disable-next-line no-unused-vars
   const [color, setColor] = useState('black');
   return (
     <div className="Parent">
-      {/* <Baby /> */}
-      <Baby key="1" className={'baby1'} parentColor={color} babyColor={'yellow'} func={setColor} />
-      <Baby key="2" className={'baby2'} parentColor={color} babyColor={'red'} func={setColor} />
-      <Baby key="3" className={'baby3'} parentColor={color} babyColor={'green'} func={setColor} />
+      <Baby parentColor={color} babyColor={'yellow'} func={setColor} />
+      <Baby parentColor={color} babyColor={'red'} func={setColor} />
+      <Baby parentColor={color} babyColor={'green'} func={setColor} />
     </div>
   );
 };
