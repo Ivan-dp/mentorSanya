356a5302ee943196e6dd375e06b4d91701165056
Ivan-dp <ivan-dp@bk.ru>
2022-05-22 18:23:47 +0300

    #26 created new pages with react-router-dom

---
+++ react-course/src/pages/index.js
@@ -0,0 +1,9 @@
+import { Home } from './Home.jsx';
+import { Info } from './Info.jsx';
+import { NotFound } from './NotFound.jsx';
+import { ToggleSquare } from './ToggleSquare.jsx';
+
+export { Home };
+export { Info };
+export { NotFound };
+export { ToggleSquare };
