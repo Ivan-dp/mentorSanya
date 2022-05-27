94fe7d9fa21267a0566f3fbf6b53e407990f49d7
Ivan-dp <ivan-dp@bk.ru>
2022-05-27 22:59:51 +0300

    #34 modified requestinput.jsx. deleted second input

--- react-course/src/pages/RequestInput.jsx
+++ react-course/src/pages/RequestInput.jsx
@@ -1,25 +1,25 @@
 import { React, useState } from 'react';
-// eslint-disable-next-line no-unused-vars
-import { getPostById } from '../functions';
 import { Post } from '../components';
+import { getPostById } from '../functions';
 
 const RequestInput = () => {
-  // eslint-disable-next-line no-unused-vars
   const [start, setStart] = useState('');
-  const [end, setEnd] = useState('');
   const [posts, setPosts] = useState([]);
 
   return (
     <div>
-      <form>
+      <form method="get">
         <label>
           Запрос поста по номеру:
           <h1>{start}</h1>
-          <input type="text" value={start} onChange={(event) => setStart(event.target.value)} />
-          <h1>{end}</h1>
-          <input type="text" value={end} onChange={(event) => setEnd(event.target.value)} />
+          <input
+            type="text"
+            name="start"
+            value={start}
+            onChange={(event) => setStart(event.target.value)}
+          />
         </label>
-        <input type="submit" value="Отправить" onClick={getPostById(start, end, setPosts)} />
+        <input type="submit" value="Отправить" onClick={getPostById(start, start + 1, setPosts)} />
       </form>
       <div>
         <ul className="post-list">
