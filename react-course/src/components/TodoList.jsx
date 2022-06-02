4d04457e925c13887dc213e1daa95bd24ae7c175
Ivan-dp <ivan-dp@bk.ru>
2022-06-02 21:54:53 +0300

    #42 add function addItem an hooks for adding new item at items array

--- react-course/src/components/TodoList.jsx
+++ react-course/src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Input, Button, Grid, Container, List } from '@mantine/core';
 import { Item } from './Item.jsx';
@@ -7,20 +7,43 @@ const TodoList = (props) => {
   TodoList.propTypes = {
     items: PropTypes.array.isRequired,
   };
+
+  const [items, setItems] = useState([...props.items]); // setItems должен прибавлять новый item={} в items=[...]
+  const [item, setItem] = useState({});
+
+  function addItem() {
+    if (item.title.length > 0) {
+      setItems([...items, item]);
+    }
+    console.log(item);
+    return items;
+  }
+  console.log(items);
+
   return (
     <Container size={400} px={0}>
       <Grid>
         <Grid.Col span={9}>
-          <Input placeholder="Enter the task" />
+          <Input
+            placeholder="Enter the task"
+            input={item.title}
+            onChange={(event) => setItem({ id: items.length + '', title: event.target.value })}
+          />
         </Grid.Col>
         <Grid.Col span={3}>
-          <Button>+</Button>
+          <Button
+            onClick={() => {
+              addItem();
+            }}
+          >
+            +
+          </Button>
         </Grid.Col>
       </Grid>
       <Grid>
         <Grid.Col>
           <List listStyleType="none" className="todo-list">
-            {props.items.map((item) => (
+            {items.map((item) => (
               <Item key={item.id} id={item.id} title={item.title} />
             ))}
           </List>
