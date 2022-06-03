936667d859276aad5e7ffb21848208a93355bf7e
Ivan-dp <ivan-dp@bk.ru>
2022-06-03 23:18:20 +0300

    #42 fixed bug abuot controlled input. Add styles for form. Input autoclear after button click or 'enter'

--- react-course/src/components/TodoList.jsx
+++ react-course/src/components/TodoList.jsx
@@ -2,6 +2,7 @@ import { Container, Grid, Input, List } from '@mantine/core';
 import PropTypes from 'prop-types';
 import { React, useState } from 'react';
 import { Item } from './Item.jsx';
+import { todoFormStyles } from '../consts/';
 
 const TodoList = (props) => {
   TodoList.propTypes = {
@@ -9,7 +10,10 @@ const TodoList = (props) => {
   };
 
   const [items, setItems] = useState([...props.items]); // setItems должен прибавлять новый item={} в items=[...]
-  const [task, setTask] = useState({});
+  const [task, setTask] = useState({
+    id: '',
+    title: '',
+  });
 
   function addItem(e) {
     if (task.title.length > 0) {
@@ -23,27 +27,29 @@ const TodoList = (props) => {
   return (
     <Container size={400} px={0}>
       <Grid>
-        <Grid.Col span={9}>
-          <Input
-            placeholder="Enter the task"
-            value={task.title}
-            onChange={(event) => {
-              setTask({ id: items.length + '', title: event.target.value });
-              event.preventDefault();
-            }}
-          />
-        </Grid.Col>
-        <Grid.Col span={3}>
-          <Input
-            component="button"
-            onClick={(event) => {
-              addItem(event);
-              setTask({ title: '' });
-            }}
-          >
-            +
-          </Input>
-        </Grid.Col>
+        <form style={todoFormStyles}>
+          <Grid.Col span={9}>
+            <Input
+              placeholder="Enter the task"
+              value={task.title}
+              onChange={(event) => {
+                setTask({ id: items.length + '', title: event.target.value });
+                event.preventDefault();
+              }}
+            />
+          </Grid.Col>
+          <Grid.Col span={3}>
+            <Input
+              component="button"
+              onClick={(event) => {
+                addItem(event);
+                setTask({ title: '' });
+              }}
+            >
+              +
+            </Input>
+          </Grid.Col>
+        </form>
       </Grid>
       <Grid>
         <Grid.Col>
--- react-course/src/consts/index.js
+++ react-course/src/consts/index.js
@@ -1,3 +1,5 @@
 import { todoItems } from './consts';
+import { todoFormStyles } from './todoFormStyles';
 
 export { todoItems };
+export { todoFormStyles };
