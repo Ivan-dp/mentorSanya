4cd66f6d6a8adafadecc5c851f637b0377c02a52
Ivan-dp <ivan-dp@bk.ru>
2022-06-05 00:06:24 +0300

    #44  1) todoItems.js: add new property 'checked'. 2) Item.jsx: add new props 'checked'. 3) Item.jsx: add checkbox.

--- react-course/src/components/Item.jsx
+++ react-course/src/components/Item.jsx
@@ -1,15 +1,22 @@
-import { List } from '@mantine/core';
-import React from 'react';
+import { List, Checkbox } from '@mantine/core';
+import { React, useState } from 'react';
 import PropTypes from 'prop-types';
 
 const Item = (props) => {
+  const [check, setCheck] = useState(props.checked);
+
   Item.propTypes = {
     id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     title: PropTypes.string.isRequired,
+    checked: PropTypes.bool.isRequired,
   };
   return (
-    <List.Item>
-      {props.id}. {props.title}
+    <List.Item className="todo-item">
+      <Checkbox
+        checked={check}
+        label={props.id + '. ' + props.title}
+        onChange={(event) => setCheck(event.currentTarget.checked)}
+      />
     </List.Item>
   );
 };
