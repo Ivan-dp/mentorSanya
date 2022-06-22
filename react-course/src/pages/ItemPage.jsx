58483359730cff1fed0957a8d1e8ca47fa8895c8
Ivan-dp <ivan-dp@bk.ru>
2022-06-22 22:33:34 +0300

    #59 add condition about rendering page if id === undefined

--- react-course/src/pages/ItemPage.jsx
+++ react-course/src/pages/ItemPage.jsx
@@ -12,13 +12,18 @@ const ItemPage = () => {
   const { id } = useParams();
   console.log(id);
   const items = todoItems;
-  return (
-    <div className="item-page">
-      <h4>
-        {items[id].id}. {items[id].title} is {JSON.stringify(items[id].checked)}
-      </h4>
-    </div>
-  );
+
+  if (id >= items.length) {
+    return <h1>Некорректно введён номер</h1>;
+  } else {
+    return (
+      <div className="item-page">
+        <h4>
+          {items[id].id}. {items[id].title} is {JSON.stringify(items[id].checked)}
+        </h4>
+      </div>
+    );
+  }
 };
 
 export { ItemPage };
