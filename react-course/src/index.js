3a9812702d3ec8c659eba300678eb7e40ff87497
Ivan-dp <ivan-dp@bk.ru>
2022-07-05 18:53:55 +0300

    #65 created store and start created reducer for title changing

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -4,16 +4,34 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { itemsContext } from './itemsContext';
 import { todoItems } from './consts';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
 
 function Main() {
   const [items, setItems] = useState(todoItems);
   const [title, setTitle] = useState('');
+
+  const defaultTitle = { 
+    title: '',
+  };
+
+  const reducer = (state = defaultItem, action) => {
+    switch(action.type) {
+      case 
+    }
+  };
+
+  const store = createStore(reducer, composeWithDevTools());
+
   return (
     // <React.StrictMode>
     <BrowserRouter>
-      <itemsContext.Provider value={{ items, setItems, title, setTitle }}>
-        <App />
-      </itemsContext.Provider>
+      <Provider store={store}>
+        <itemsContext.Provider value={{ items, setItems, title, setTitle }}>
+          <App />
+        </itemsContext.Provider>
+      </Provider>
     </BrowserRouter>
     // </React.StrictMode>
   );
