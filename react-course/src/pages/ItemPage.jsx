0c1602a256e1dd4dea7534e81e3bd65941cf1cdc
Ivan-dp <ivan-dp@bk.ru>
2022-07-28 12:15:59 +0300

    #72 deleted unnecessary code

--- react-course/src/pages/ItemPage.jsx
+++ react-course/src/pages/ItemPage.jsx
@@ -11,61 +11,48 @@ const ItemPage = () => {
 
   const { id } = useParams();
 
-  // for (let task of list) {
-  //   if (task.id == id) {
-  //     console.log(task);
-  //   }
-  //   return task;
-  // }
   console.log(id);
   let element;
   for (element of list) {
     if (element.id === id) {
       console.log(element);
-      // task = element;
-      // return task;
     }
     return element;
   }
   console.log(element);
-  // const [title, setTitle] = list[id].title;
 
-  if (id >= list.length) {
-    return <h1>Некорректно введён номер</h1>;
-  } else {
-    return (
-      <div className="item-page">
-        <div className="item-data">
-          <h1>{element.id}</h1>
-          <h3>{element.title}</h3>
-          <h3 className="item-bool">{JSON.stringify(element.checked)}</h3>
-        </div>
-        <form className="todo-form" style={{ width: 250 }}>
-          <Input
-            style={{ width: 200 }}
-            placeholder={element.title}
-            value={element.title}
-            onChange={(event) => {
-              element.title = event.target.value;
-            }}
-          ></Input>
-          <Input
-            style={{ width: 50 }}
-            component="button"
-            onClick={(event) => {
-              element.title = event.target.value;
-              event.preventDefault();
-            }}
-          ></Input>
-        </form>
-        <Link className="item-back-link" to="/mentorSanya/newpage/all">
-          <Button color="teal" variant="outline">
-            Go Back
-          </Button>
-        </Link>
+  return (
+    <div className="item-page">
+      <div className="item-data">
+        <h1>{element.id}</h1>
+        <h3>{element.title}</h3>
+        <h3 className="item-bool">{JSON.stringify(element.checked)}</h3>
       </div>
-    );
-  }
+      <form className="todo-form" style={{ width: 250 }}>
+        <Input
+          style={{ width: 200 }}
+          placeholder={element.title}
+          value={element.title}
+          onChange={(event) => {
+            element.title = event.target.value;
+          }}
+        ></Input>
+        <Input
+          style={{ width: 50 }}
+          component="button"
+          onClick={(event) => {
+            element.title = event.target.value;
+            event.preventDefault();
+          }}
+        ></Input>
+      </form>
+      <Link className="item-back-link" to="/mentorSanya/newpage/all">
+        <Button color="teal" variant="outline">
+          Go Back
+        </Button>
+      </Link>
+    </div>
+  );
 };
 
 export { ItemPage };
